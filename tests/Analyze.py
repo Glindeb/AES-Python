84c86919bef5e773d0912a70ca6f8df25ff06f7f
Gabriel Lindeblad <78178279+Glindeb@users.noreply.github.com>
2022-07-17 23:06:37 +0200

    First test on how to visualize data

--- tests/Analyze.py
+++ tests/Analyze.py
@@ -3,14 +3,14 @@ import AES_Module.AES as AES
 def enc():
     key = "2b7e151628aed2a6abf7158809cf4f3c"
     running_mode = "ECB"
-    file_path = r"C:\Users\Gabriel\Documents\GitHub\AES-Python\tmp\test_files\data.txt"
+    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/data.txt"
 
     AES.encrypt(key, file_path, running_mode)
 
 def dec():
     key = "2b7e151628aed2a6abf7158809cf4f3c"
     running_mode = "ECB"
-    file_path = r"C:\Users\Gabriel\Documents\GitHub\AES-Python\tmp\test_files\data.txt.enc"
+    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/data.txt.enc"
 
     AES.decrypt(key, file_path, running_mode)
 
--- tmp/test_2.py
+++ tmp/test_2.py
@@ -1,71 +1,22 @@
-# nk = number of colums, nr = number of rounds
-def Key_schedule(key, nk, nr):
-    # Create list and populates first nk words with key
-    words = [(key[4*i], key[4*i+1], key[4*i+2], key[4*i+3]) for i in range(nk)]
+from matplotlib import pyplot as plt
+import numpy as np
 
-    # fill out the rest based on previews words, rotword, subword and rcon values
-    limit = False
-    for i in range(nk, (nr * nk)):
-        # get required previous keywords
-        temp, word = words[i-1], words[i-nk]
-
-        # if multiple of nk use rot, sub, rcon etc
-        if i % nk == 0:
-            x = SubWord(RotWord(temp))
-            rcon = round_constant[int(i/nk)]
-            temp = hexor(x, hex(rcon)[2:])
-            limit = False
-        elif i % 4 == 0:
-            limit = True
-
-        if i % 4 == 0 and limit and nk >= 8:
-            temp = SubWord(temp)
-
-        # creating strings of hex rather than tuple
-        word = ''.join(word)
-        temp = ''.join(temp)
-
-        # xor the two hex values
-        xord = hexor(word, temp)
-        words.append((xord[:2], xord[2:4], xord[4:6], xord[6:8]))
-    return words
+# Set the figure size
+plt.rcParams["figure.figsize"] = [5.00, 5.00]
+plt.rcParams["figure.autolayout"] = True
 
 
-def Keyschedule_256bit(key):
-    # prep word list to hold 60 tuples
-    words = [()]*60
+# Random data points
+with open("/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/data.txt", "rb") as f:
+    data = f.read()
+    data = [i for i in data]
+    data = [data[x:x+32] for x in range(0, len(data), 32)]
+    #data = data[:-1]
+print(data)
 
-    # fill out first 8 words based on the key
-    for i in range(8):
-        words[i] = (key[4*i], key[4*i+1], key[4*i+2], key[4*i+3])
 
-    # fill out the rest based on previews words, rotword, subword and rcon values
-    limit = False
-    for i in range(8, 60):
-        # get required previous keywords
-        temp = words[i-1]
-        word = words[i-8]
+# Plot the data using imshow with gray colormap
+plt.imshow(data, interpolation=None, cmap="gray")
 
-        # if multiple of 4 use rot, sub, rcon etc
-        if i % 8 == 0:
-            print("f")
-            x = RotWord(temp)
-            y = SubWord(x)
-            rcon = round_constant[int(i/8)]
-            temp = hexor(y, hex(rcon)[2:])
-            limit = False
-        elif i % 4:
-            limit = True
-
-        if i % 4 == 0 and limit:
-            print("g")
-            temp = SubWord(temp)
-
-        # creating strings of hex rather than tuple
-        word = ''.join(word)
-        temp = ''.join(temp)
-
-        # xor the two hex values
-        xord = hexor(word, temp)
-        words[i] = (xord[:2], xord[2:4], xord[4:6], xord[6:8])
-    return words
+# Display the plot
+plt.show()
