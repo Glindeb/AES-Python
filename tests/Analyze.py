af23ab132cb7b9a3536f3b9fd5496c92d718a0eb
Gabriel Lindeblad <78178279+Glindeb@users.noreply.github.com>
2022-07-20 22:36:28 +0200

    generated a third image for comparison in report

--- tests/Analyze.py
+++ tests/Analyze.py
@@ -5,7 +5,7 @@ def main(i):
     key = "2b7e151628aed2a6abf7158809cf4f3c"
     iv = "000102030405060708090a0b0c0d0e0f"
     running_mode = "CBC"
-    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/data.txt"
+    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tests/test_data/File_enc_visualization_3/body.bin"
 
     if i == "enc":
         AES.encrypt(key, file_path, running_mode, iv)
