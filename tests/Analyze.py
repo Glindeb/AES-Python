f49d422ffbf0d2940a17d1defb13ac3daeb5af77
Gabriel Lindeblad <78178279+Glindeb@users.noreply.github.com>
2022-07-20 17:13:54 +0200

    new visualization method to show weakness of ecb

--- tests/Analyze.py
+++ tests/Analyze.py
@@ -3,17 +3,17 @@ import AES_Module.AES as AES
 def enc():
     key = "2b7e151628aed2a6abf7158809cf4f3c"
     running_mode = "ECB"
-    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/data.txt"
+    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/body.bin"
 
     AES.encrypt(key, file_path, running_mode)
 
 def dec():
     key = "2b7e151628aed2a6abf7158809cf4f3c"
     running_mode = "ECB"
-    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/data.txt.enc"
+    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/body.bin.enc"
 
     AES.decrypt(key, file_path, running_mode)
 
 if __name__ == '__main__':
-    dec()
+    enc()
 
--- tmp/test_2.py
+++ tmp/test_2.py
@@ -1,5 +1,4 @@
 from matplotlib import pyplot as plt
-import numpy as np
 
 # Set the figure size
 plt.rcParams["figure.figsize"] = [5.00, 5.00]
@@ -7,16 +6,26 @@ plt.rcParams["figure.autolayout"] = True
 
 
 # Random data points
-with open("/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/data.txt", "rb") as f:
+#with open("/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/data.txt.enc", "rb") as f:
+#    data = f.read().splitlines()
+#    print(data)
+#    for index, pice in enumerate(data):
+#        data[index] = [i for i in pice]
+
+with open("/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/data.txt.enc", "rb") as f:
     data = f.read()
     data = [i for i in data]
-    data = [data[x:x+32] for x in range(0, len(data), 32)]
+    data = [data[x:x+192] for x in range(0, len(data), 192)]
     #data = data[:-1]
 print(data)
 
+print(data)
 
-# Plot the data using imshow with gray colormap
-plt.imshow(data, interpolation=None, cmap="gray")
+
+plt.title('adjust axis scale')
+
+# Plot the data using imshow with gray colormap & aspect set to auto
+plt.imshow(data, interpolation=None, cmap="gray", aspect="auto")
 
 # Display the plot
 plt.show()
