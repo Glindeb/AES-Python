fa02759b9d3a8e02eef3d83ce734d11fd17923fe
Gabriel Lindeblad <78178279+Glindeb@users.noreply.github.com>
2022-09-01 12:17:50 +0200

    Fix for inefficeny problem key expantion

--- tmp/test_3.py
+++ tmp/test_3.py
@@ -1,14 +1,14 @@
-from PyAES.AES import ofb_dec, ofb_enc
+from PyAES.AES import ecb_dec, ecb_enc
 
 def run(val):
     key = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"
     iv = "000102030405060708090a0b0c0d0e0f"
-    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/data.txt"
+    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/sword_fifth_bakground_light_1.jpg"
 
     if val == 1:
-        ofb_enc(key, file_path, iv)
+        ecb_enc(key, file_path)
     elif val == 2:
-        ofb_dec(key, f"{file_path}.enc", iv)
+        ecb_dec(key, f"{file_path}.enc")
 
 if __name__ == "__main__":
-    run(2)
+    run(1)
