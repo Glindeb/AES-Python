cda3326ad0a6310e311e25dabb794b0160d9695f
Gabriel Lindeblad <78178279+Glindeb@users.noreply.github.com>
2022-09-01 20:45:28 +0200

    Fixed visual representation for encryption

--- tmp/test_3.py
+++ tmp/test_3.py
@@ -1,14 +1,14 @@
-from PyAES.AES import ecb_dec, ecb_enc
+from PyAES.AES import ofb_dec, ofb_enc
 
 def run(val):
-    key = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"
-    iv = "000102030405060708090a0b0c0d0e0f"
-    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tmp/test_files/sword_fifth_bakground_light_1.jpg"
+    key = "0" * 32
+    iv = "0" * 32
+    file_path = r"/Users/gabriellindeblad/Documents/GitHub/AES-Python/tests/test_data/Visual-for-report/body.bin"
 
     if val == 1:
-        ecb_enc(key, file_path)
+        ofb_enc(key, file_path, iv)
     elif val == 2:
-        ecb_dec(key, f"{file_path}.enc")
+        ofb_dec(key, f"{file_path}.enc", iv)
 
 if __name__ == "__main__":
     run(1)
