cd75c452a49508935e2824fcd6ddd4573d28e76e
Gabriel Lindeblad <78178279+Glindeb@users.noreply.github.com>
2023-01-25 10:00:10 +0100

    finaliesd some things about structure explanaition

--- src/PyAES/AES.py
+++ src/PyAES/AES.py
@@ -182,26 +182,26 @@ def encryption_rounds(data, round_keys, nr):
 # This function is used for the decryption of data matrixes
 # using the expanded keys.
 def decryption_rounds(data, round_keys, nr):
-    # Inizial add round key
+    # Inizial add round key, inverse shift rows and inverse sub bytes
     data = np.bitwise_xor(data, round_keys[-1])
+    data = inv_shift_rows(data)
+    data = sub_bytes(data, invSubBytesTable)
 
     # Rounds 1 to 9 or 1 to 11 or 1 to 13
     # Here each step in one round is performed in a sequence n times
     # where n is the number of rounds minus the last round.
     for i in range(1, (nr - 1)):
-        # Inverse shift rows
-        data = inv_shift_rows(data)
-        # Inverse sub bytes
-        data = sub_bytes(data, invSubBytesTable)
         # Add round key
         data = np.bitwise_xor(data, round_keys[-(i+1)])
         # Inverse mix columns
         data = inv_mix_columns(data)
+        # Inverse shift rows
+        data = inv_shift_rows(data)
+        # Inverse sub bytes
+        data = sub_bytes(data, invSubBytesTable)
 
     # Final round
-    # Identical to the previous rounds, but without mix columns
-    data = inv_shift_rows(data)
-    data = sub_bytes(data, invSubBytesTable)
+    # Final add round key of final round
     data = np.bitwise_xor(data, round_keys[0])
 
     # Returns the decrypted data
