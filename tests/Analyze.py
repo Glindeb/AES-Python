b4af1f5d3f2dded0ff9ed0532cbb81adf8e6ff78
Gabriel Lindeblad <gabriel.lindeblad@icloud.com>
2023-02-15 09:01:48 +0100

    Dev (#56)
    * build file
    
    * generated test results
    
    * Written more on teori and method
    
    * fix to test setup output
    
    * implemented progressbar in test script
    
    * .
    
    * Result from new run moved and generated
    
    * Added appendix and reformated the report some
    
    * small fixes to text
    
    * minor moves forwad on teori
    
    * new figures and changes all over the place
    
    * minor fix to avgränsing
    
    * fixed a scentence
    
    * worked on bakgrund
    
    * background some what completed
    
    * resultat sammanställt plus lite till
    
    * worked on teori and method as well as result
    
    * Completed parts of method
    
    * added some comments
    
    * minor updates to method, starting to add comments
    
    * added comments to the entire analyze file
    
    * Minor changes
    
    * Added more verbose comments to aes.py
    
    * fixed minor wording issue
    
    * work on metod
    
    * wrote nyckellängds test part
    
    * metohd done and some sources corected
    
    * fixed figures...
    
    * commented some in the aes.py file
    
    * commented some in aes.py
    
    * finaliesd some things about structure explanaition
    
    * Started writing on subbytes
    
    * fixed quite a lot at the teori
    
    * started work on key expantion section
    
    * fixed first part of keyexpansion
    
    * almost completed teori part of the report
    
    * teori completed
    
    * Began writing in disskusion
    
    * disskussion writing
    
    * writing on disskussion
    
    * version number correction

--- tests/Analyze.py
+++ tests/Analyze.py
@@ -1,86 +1,148 @@
+# ---------------------------------------------------------------
+#                   Background structure
+# ---------------------------------------------------------------
+
+# Imports the encrypt function from the PyAES module
 from PyAES.encrypt import encrypt
-from timeit import default_timer as timer
+# Imports the timer and random number generator
+from time import perf_counter
 from random import randint
+# Imports the remove function for deleting files
 from os import remove
-import csv
-from multiprocessing import Process
 
-end_f = 'B.txt'
 
-def test_init(mode, number, name_f):
-    def setup(count, name_f):
-        with open(name_f + str(count) + end_f, 'wb') as f:
+# For displaying progress during test runs
+def progress_bar(progress, total_progress):
+    percent = 100 * (float(progress) / float(total_progress))
+    bar_progress = int(100 * (float(progress) / float(total_progress)))
+
+    if bar_progress > 100 or percent > 100:
+        bar_progress = 100
+        percent = 100
+
+    bar_remaining = 100 - bar_progress
+    bar = '#' * bar_progress + '-' * bar_remaining
+    print(f"\r[{bar}] {percent:.2f}%", end="\r")
+    return progress + 1
+
+
+# Writes the resulting data to a text file
+def write_data(data, name_f):
+    # Creates a text file with the specified name
+    with open(name_f + '.txt', 'w') as f:
+        for i in data:
+            # Writes the data to the text file
+            f.write(str(i) + '\n')
+        f.write(str('\n'))
+
+
+# Creates a text file with specified size and fills it with random bytes
+def setup(count):
+        # Creates a text file with the name test_speed.txt
+        with open("test_speed.txt", 'wb') as f:
             for j in range(count):
+                # Writes random bytes to the text file
                 f.write(bytes([randint(0, 255)]))
 
-    def clean(count, name_f):
-        remove(name_f + str(count) + end_f + '.enc')
 
-    if mode == 'setup':
-        setup(number, name_f)
-    elif mode == 'clean':
-        clean(number, name_f)
+# Runs the specified function and returns the time it takes to run
+def speed_test(count, key, mode, iv):
+    # Creates a text file with the specified size
+    setup(count)
+    # Starts the timer
+    start = perf_counter()
+    # Executes the function
+    encrypt(key, "test_speed.txt", mode, iv=iv)
+    # Stops the timer
+    end = perf_counter()
+    # Deletes the text file
+    remove("test_speed.txt.enc")
+    return end - start
 
-
-def write_data(data, name_f):
-    with open(name_f + '.csv', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-
-
-def test_encrypt(name, key, sizes, running_mode, iv=None, iterations=None):
-    test = []
-    for i in range(iterations):
-        test_0 = []
-        for i in range(len(sizes)):
-            test_init('setup', sizes[i], name)
-            start = timer()
-            encrypt(key, name + str(sizes[i]) + end_f, running_mode, iv=iv)
-            end = timer()
-            test_init('clean', sizes[i], name)
-            test_0.append(end - start)
-        test.append(test_0)
-    write_data(test, name)
-
-
+# Executes the code if the file is run directly
 if __name__ == '__main__':
-    # Run in parallel
-    key_128 = "2b7e151628aed2a6abf7158809cf4f3c"
-    key_192 = "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b"
-    key_256 = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"
+    # Test parameters
+    keys = ["2b7e151628aed2a6abf7158809cf4f3c",
+            "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b",
+    "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"]
     iv = "000102030405060708090a0b0c0d0e0f"
-    sizes = [(2**3), (2**5), (2**7), (2**9), (2**11), (2**13), (2**15), (2**17), (2**19), (2**21), (2**23), (2**25)]
+    file_size = 1000000
+    runs = 25
 
-    p1 = Process(target=test_encrypt, args=("test_1_ECB", key_128, sizes, 'ECB', None, 10))
-    p2 = Process(target=test_encrypt, args=("test_1_CBC", key_128, sizes, 'CBC', iv, 10))
-    p3 = Process(target=test_encrypt, args=("test_1_OFB", key_128, sizes, 'OFB', iv, 10))
+    # ---------------------------------------------------------------
+    #     Test time difference between 128, 192 and 256 bit keys
+    # ---------------------------------------------------------------
+    data = []
+    progress = 0
+    # Loops through the different key sizes
+    for i in keys:
+        data_tmp = []
+        # Runs the test the specified amount of times
+        for j in range(runs):
+            # Displays the progress
+            progress = progress_bar(progress, 150)
+            # Runs the test and saves the result
+            test = speed_test(file_size, i, 'ECB', iv)
+            # Saves the result
+            data_tmp.append(test)
+        # Writes the the results of every run to a text file
+        write_data(data_tmp, 'keys_test_raw ' + str(len(i) * 4))
+        # Saves the average of the results
+        data.append(sum(data_tmp)/len(data_tmp))
+    # Writes the average of the results to a text file
+    write_data(data, 'keys_test')
 
-    p4 = Process(target=test_encrypt, args=("test_2_ECB", key_192, sizes, 'ECB', None, 10))
-    p5 = Process(target=test_encrypt, args=("test_2_CBC", key_192, sizes, 'CBC', iv, 10))
-    p6 = Process(target=test_encrypt, args=("test_2_OFB", key_192, sizes, 'OFB', iv, 10))
+    # ---------------------------------------------------------------
+    #      Test time difference between ECB, CBC and OFB modes
+    # ---------------------------------------------------------------
+    data = []
+    data_tmp = []
 
-    p7 = Process(target=test_encrypt, args=("test_3_ECB", key_256, sizes, 'ECB', None, 10))
-    p8 = Process(target=test_encrypt, args=("test_3_CBC", key_256, sizes, 'CBC', iv, 10))
-    p9 = Process(target=test_encrypt, args=("test_3_OFB", key_256, sizes, 'OFB', iv, 10))
+    # Runs the time test for OFB the specified amount of times
+    for i in range(runs):
+        # Displays the progress
+        progress = progress_bar(progress, 150)
+        # Runs the test for OFB
+        test = speed_test(file_size, keys[0], 'OFB', iv)
+        # Saves the result in a temporary list
+        data_tmp.append(test)
+    # Writes the the results of every run to a text file
+    write_data(data_tmp, 'modes_test_raw OFB')
+    # Saves the average of the results in a list and clears the temporary list
+    data.append(sum(data_tmp)/len(data_tmp))
+    data_tmp = []
 
-    p1.start()
-    p2.start()
-    p3.start()
-    p4.start()
-    p5.start()
-    p6.start()
-    p7.start()
-    p8.start()
-    p9.start()
+    # Runs the time test for CBC the specified amount of times
+    for i in range(runs):
+        # Displays the progress
+        progress = progress_bar(progress, 150)
+        # Runs the test for CBC
+        test = speed_test(file_size, keys[0], 'CBC', iv)
+        # Saves the result in a temporary list
+        data_tmp.append(test)
+    # Writes the the results of every run to a text file
+    write_data(data_tmp, 'modes_test_raw CBC')
+    # Saves the average of the results in a list and clears the temporary list
+    data.append(sum(data_tmp)/len(data_tmp))
+    data_tmp = []
 
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-    p5.join()
-    p6.join()
-    p7.join()
-    p8.join()
-    p9.join()
+    # Runs the time test for ECB the specified amount of times
+    for i in range(runs):
+        # Displays the progress
+        progress = progress_bar(progress, 150)
+        # Runs the test for ECB
+        test = speed_test(file_size, keys[0], 'ECB', iv)
+        # Saves the result in a temporary list
+        data_tmp.append(test)
+    # Writes the the results of every run to a text file
+    write_data(data_tmp, 'modes_test_raw ECB')
+    # Saves the average of the results in a list
+    data.append(sum(data_tmp)/len(data_tmp))
+    # Writes the averages from every run to a text file
+    write_data(data, 'modes_test')
 
-    print("Done")
+    # Dilsapys the progress finished
+    progress = progress_bar(progress, 150)
+    # Prints that the tests is completed
+    print("\n")
+    print("Completed")
